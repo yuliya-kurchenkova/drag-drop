f8a30d255a8b4823907d8e6c000d98b9e7d2b0e2
yuliya-kurchenkova <68619292+yuliya-kurchenkova@users.noreply.github.com>
2021-09-20 14:53:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+for(const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover);
+    placeholder.addEventListener('dragenter', dragenter);
+    placeholder.addEventListener('dragleave', dragleave);
+    placeholder.addEventListener('drop', dragdrop);
+}
+
+function dragover(event) {
+    event.preventDefault()
+}
+
+function dragenter(event) {
+    event.target.classList.add('hoved');
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hoved')
+}
+
+function dragdrop(event) {
+    event.target.append(item);
+}
+
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
+
+function dragstart(event) {
+    event.target.classList.add('hold');
+    setTimeout(() => {
+        event.target.classList.add('hide');
+    }, 0)
+}
+
+function dragend(event) {
+    event.target.classList.remove('hold', 'hide');
+}
